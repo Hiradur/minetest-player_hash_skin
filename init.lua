79cd0eceb0794a4ba491832610707a7f50ae43e5
xisd <zlexis.dnl@googlemail.com>
2016-10-04 07:57:46 +0200

    auto count and simple_skins support
    Loop through textures file to count them = super easy to add new textures, its just a matter of placing file in the right folder with the right name
    Support for simple_skins mod = more textures and ability to change in game

--- init.lua
+++ init.lua
@@ -3,17 +3,58 @@ minetest.register_on_joinplayer(function(player)
   local len = string.len(name)
   local hash = 0
   
+	h = {}
+  -- prefix of textures files
+	h.sprefix = "player_hash_skin_"
+  -- mod providing textures files
+	h.smodpath = minetest.get_modpath("player_hash_skin")
+  -- start count of texture files
+	h.s = 0
+  -- number of texture file
+	h.n = 8
+
   for i = 1, len, 1 do
    hash = hash+(string.byte(name,i))
   end
-  
-  hash = hash%8
-   
-	local filename = minetest.get_modpath("player_hash_skin").."/textures/".."player_hash_skin_"..hash
-	local f = io.open(filename..".png")
 	
+  
+	-- if simple_skins mod exist, use its skins
+	local skinmod = minetest.get_modpath("simple_skins")
+	if skinmod ~= nil then
+	 	minetest.log("action", "[player_hash_skin] simple_skins mod detected")
+		 h.smodpath = minetest.get_modpath("simple_skins")
+		 h.sprefix = "character_"
+		 h.s = 1
+	end
+
+	
+	-- loop through the files and count them  
+	h.n = h.s
+ 	minetest.log("action", "n =  "..h.n..".")
+	local fp
+	while true do
+		fp = io.open(h.smodpath .. "/textures/".. h.sprefix ..h.n.. ".png")
+		if not fp then break end
+		fp:close()
+		h.n = h.n + 1
+	end
+	-- if count started at 1 need to remove 1
+	h.n = h.n - h.s
+	
+ 	minetest.log("action", "[player_hash_skin] "..h.n.." skins found")
+
+	-- hash
+ 	hash = hash%h.n
+ 	minetest.log("action", "[player_hash_skin] hash =  "..hash..".")
+
+
+	-- ouvre le fichier
+	local filename = h.smodpath.."/textures/"..h.sprefix..hash
+	local f = io.open(filename..".png")
+		
 	if f then
 		f:close()
-		default.player_set_textures(player, {string.format("player_hash_skin_%i.png", hash)})
+		local img = h.sprefix.."%i.png"
+		default.player_set_textures(player, {string.format(img, hash)})
 	end
 end)
